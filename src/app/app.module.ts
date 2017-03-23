144c10d3b05a61adf73f36f9ddfde041f64b16d8
Ralf Brunner <ralf.brunner@device-insight.com>
2017-03-23 10:01:29 +0100

    Settings page base setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
 
-import { AppComponent } from './app.component';
-import { myRequestOptionsProvider } from "./default-request-options.service";
-import { AlertComponent } from './alert/alert.component';
-import { AlertService } from "./alert/alert.service";
-import { TimesheetComponent } from './timesheet/timesheet.component';
+import {AppComponent} from "./app.component";
+import {myRequestOptionsProvider} from "./default-request-options.service";
+import {AlertComponent} from "./alert/alert.component";
+import {AlertService} from "./alert/alert.service";
+import {TimesheetComponent} from "./timesheet/timesheet.component";
 import "rxjs/add/operator/finally";
 import "rxjs/add/operator/map";
 import "rxjs/add/observable/forkJoin";
-import { DatepickerComponent } from './datepicker/datepicker.component';
+import {DatepickerComponent} from "./datepicker/datepicker.component";
 import {TimesheetService} from "./timesheet/service/timesheet.service";
 import {HarvestService} from "./timesheet/service/harvest.service";
 import {JiraService} from "./timesheet/service/jira.service";
-import { SpinnerComponent } from './spinner/spinner.component';
+import {SpinnerComponent} from "./spinner/spinner.component";
 import {SpinnerService} from "./spinner/spinner.service";
+import {Router} from "@angular/router";
+import {AppRoutingModule} from "./app-routing.module";
+import {MainSyncOverviewComponent} from "./main-sync-overview/main-sync-overview.component";
+import {OptionsComponent} from "./options/options.component";
 
 
 @NgModule({
@@ -25,14 +29,23 @@ import {SpinnerService} from "./spinner/spinner.service";
     AlertComponent,
     TimesheetComponent,
     DatepickerComponent,
-    SpinnerComponent
+    SpinnerComponent,
+    MainSyncOverviewComponent,
+    OptionsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [myRequestOptionsProvider, AlertService, SpinnerService, TimesheetService, HarvestService, JiraService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  // Diagnostic only: inspect router configuration
+  constructor(router: Router) {
+    console.debug('Routes: ', JSON.stringify(router.config, undefined, 2));
+  }
+}
---
+++ src/app/main-sync-overview/main-sync-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-sync-overview',
+  templateUrl: './main-sync-overview.component.html',
+  styleUrls: ['./main-sync-overview.component.css']
+})
+export class MainSyncOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
